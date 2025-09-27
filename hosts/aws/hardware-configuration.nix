c508da33653a5c4918aa62e74fc9a8e2fad16a1a
Felipe Tejada <felipe.tejada@kommit.co>
2025-09-27 18:41:41 -0500

    fix: update git user configuration to correct username

--- hosts/aws/hardware-configuration.nix
+++ hosts/aws/hardware-configuration.nix
@@ -30,6 +30,6 @@
   users.users.root.openssh.authorizedKeys.keys =
   [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOxue2KK9duORxcXOsvguCJ47NuT/lY6ZhSG+RroLa8isH+z+inXQfS/AGoUAPKQ2DSp4qNIMxcH4RoXTOZZFeYSqAnVcjbSQbq8aK5m0g2U41icOeGo/B5lSiSi2CWXEyombcA/1rU8coq0+XGhsemWpU0oaYIzH1ZvVMiRGc5uBhGUbP06jslWAmilyZc0zCRSBzKyLmUqKHibqrUBXvL4UN3MmJ10IZdjCoTXtUqc9KB52HDifEe3pnGlB4OkpscRmeAPs6mGi8qUDmL0DlKWjAIdZK0xTpd+dgHIMDg6iMJJUjE1lDZUM5zggj7g8RAN2sPQQS5gDM0SvOpY/N"
-  ] ++ (args.extraPublicKeys or []);
+  ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
