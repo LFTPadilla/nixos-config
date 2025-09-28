c712581968617211f9e1ddda0cee3e67965f032c
Felipe Tejada <felipe.tejada@kommit.co>
2025-09-27 19:05:18 -0500

    fix: comment out unused imports in default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,34 +1,34 @@
 { ... }:
 {
   imports = [
-    ./aseprite/aseprite.nix           # pixel art editor
-    ./audacious/audacious.nix         # music player
+    # ./aseprite/aseprite.nix           # pixel art editor
+    # ./audacious/audacious.nix         # music player
     ./bat.nix                         # better cat command
     ./browser.nix                     # firefox based browser
     ./btop.nix                        # resouces monitor
-    ./cava.nix                        # audio visualizer
-    ./discord.nix                     # discord
+    # ./cava.nix                        # audio visualizer
+    # ./discord.nix                     # discord
     ./fastfetch/fastfetch.nix         # fetch tool
     ./flow.nix                        # terminal text editor
     ./fzf.nix                         # fuzzy finder
-    ./gaming.nix                      # packages related to gaming
+    # ./gaming.nix                      # packages related to gaming
     ./ghostty/ghostty.nix             # terminal
     ./git.nix                         # version control
     ./gnome.nix                       # gnome apps
     ./gtk.nix                         # gtk theme
     ./hyprland                        # window manager
     ./kitty.nix                       # terminal
-    ./lazygit.nix
+    # ./lazygit.nix
     ./micro.nix                       # nano replacement
     ./nemo.nix                        # file manager
-    ./nix-search/nix-search.nix       # TUI to search nixpkgs
+    # ./nix-search/nix-search.nix       # TUI to search nixpkgs
     ./nvim.nix                        # neovim editor
-    ./obsidian.nix
+    # ./obsidian.nix
     ./p10k/p10k.nix
     ./packages                        # other packages
-    ./retroarch.nix
+    # ./retroarch.nix
     ./rofi/rofi.nix                   # launcher
-    ./scripts/scripts.nix             # personal scripts
+    # ./scripts/scripts.nix             # personal scripts
     ./ssh.nix                         # ssh config
     ./superfile/superfile.nix         # terminal file manager
     ./swaylock.nix                    # lock screen
